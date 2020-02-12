7217efa72d98ab5384348ba5cdaad6e3eccbf285
Alicia Burn <AliciaBurn@gmail.com>
2020-02-12 07:37:51 -0500

    Working on server.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,6 +4,18 @@ const router = require("express").Router()
 
 router.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"))
-})
+});
+
+router.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/assets/js/index.js"))
+});
+
+router.get("*", function(req,res) {
+    res.sendFile(path.join(__dirname, "/assets/css/style.css"))
+});
+
+router.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"))
+});
 
 module.exports = router
