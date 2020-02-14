cff91e132e5ef946a505cb253216a10f10546a6d
Alicia Burn <AliciaBurn@gmail.com>
2020-02-13 22:03:58 -0500

    Try something else

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,17 +3,17 @@ var path = require("path");
 module.exports = function(app) {
 
     app.get("/notes", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/notes.html"));
+        res.sendFile(path.join(__dirname, "public/notes.html"));
     });
 
     app.get("../public/assets/js/index.js", function(req,res){
-        res.sendFile(path.join(__dirname, "../public/assets/js/index.js"))
+        res.sendFile(path.join(__dirname, "public/assets/js/index.js"))
     });
     app.get("../public/assets/css/style.css", function(req,res){
-        res.sendFile(path.join(__dirname, "../public/assets/css/style.css"))
+        res.sendFile(path.join(__dirname, "public/assets/css/style.css"))
     });
 
     app.get("*", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
+        res.sendFile(path.join(__dirname, "public/index.html"));
       });
 }
